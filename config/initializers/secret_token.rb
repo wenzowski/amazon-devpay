e0f96dbc7b3321ed4351be252e43645c49297820
Alexander Wenzowski <alexander@wenzowski.com>
2012-05-16 19:36:06 -0400

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazonDevpay::Application.config.secret_token = 'ff2e8b8b13e37df37b051727a8f881ccf2a8efacc26b4568967acf2efa9f26e2672fd68123058a7d36c2301f1ae215a3c1e6d02a188e87925a642062341450f7'
